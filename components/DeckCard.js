e25c7d5a09e38d6c5bd0bd4d0b9b09a16418a943
Chris Paredes <chrisparedes206@gmail.com>
2018-09-18 15:58:32 -0700

    Move home view deck card to its own component

---
+++ components/DeckCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native'
+
+export default class DeckCard extends React.Component {
+  render() {
+    console.log("DeckCard props", this.props)
+    // const { deck } = this.props
+    const { title, questions } = this.props
+
+    return (
+      <View style={styles.deck}>
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{fontSize: 24}}>{title}</Text>
+          <Text style={{fontSize: 18, color: '#666666'}}>
+            {questions && questions.length} cards
+          </Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  deck: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    margin: 5,
+    padding: 14,
+    borderRadius: 5,
+    height: 65,
+    borderColor: '#000',
+    width: 320,
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
--- components/Home.js
+++ components/Home.js
@@ -4,6 +4,7 @@ import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated, 
 import { purple, white, red } from '../utils/colors'
 import { getDecksFromStorage } from '../utils/api'
 import { receiveDecks } from '../actions'
+import DeckCard from './DeckCard'
 
 class Home extends Component {
   componentDidMount() {
@@ -15,31 +16,21 @@ class Home extends Component {
   }
 
   renderDeck = ({item}) => (
-    <View style={styles.item}>
+    <View>
       <TouchableOpacity onPress={() =>
         this.props.navigation.navigate('DeckHome', item)}>
-
-        <View style={styles.deck}>
-          <View style={{justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={{fontSize: 20}}>{item.title}</Text>
-            <Text style={{fontSize: 16, color: '#b7bdc1'}}>
-                {item.questions && item.questions.length} cards
-            </Text>
-          </View>
-        </View>
-
+        <DeckCard title={item.title} questions={item.questions} />
       </TouchableOpacity>
     </View>
   )
 
   render() {
-    console.log("Home render this.props.decks: ", this.props.decks)
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <FlatList
           data={Object.values(this.props.decks).sort((a, b) => a.title > b.title)}
           renderItem={this.renderDeck}
-          keyExtractor={(item, index) => index}/>
+          keyExtractor={(item, index) => index.toString()}/>
       </View>
     )
   }
@@ -51,20 +42,4 @@ function mapStateToProps(state) {
   }
 }
 
-const styles = StyleSheet.create({
-  deck: {
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-    margin: 5,
-    padding: 14,
-    borderRadius: 5,
-    height: 65,
-    borderColor: '#000',
-    width: 320,
-    borderWidth: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
-
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps)(Home)
